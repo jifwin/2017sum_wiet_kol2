3d7b06dece23528ba4feefaa8199fb19ac15a97e
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-03 15:15:18 +0200

    add attendance and score stub

---
+++ attendance.py
@@ -0,0 +1,4 @@
+class Attendance:
+    def __init__(self, student, date):
+        self.student = student
+        self.date = date
--- diary.py
+++ diary.py
@@ -1,2 +1,10 @@
 class Diary:
-    pass
+    def __init__(self):
+        self.scores = []
+        self.attendances = []
+
+    def add_score(self, score):
+        self.scores.append(score)
+
+    def add_attendance(self, attendance):
+        self.attendances.append(attendance)
