6200c88df022565da23e01b93de636eda3d066cc
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-16 21:50:42 +0200

    formatting

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ NUMBER_OF_ATTENDANCES = 15
 DATES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 STUDENTS = ["Adam Nowak", "Janusz Golonka", "Henryk Janowski"]
 
+
 def initialize_diary():
     diary = Diary(STUDENTS, CLASSES, DATES)
     for i in xrange(NUMBER_OF_SCORES):
